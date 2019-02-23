f43757599227f7d5fd63f68e57c24a6919b781ac
HassanGamer 555 <47833043+HassanGamer555@users.noreply.github.com>
2019-02-23 12:03:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("DS$",{type: 'PLAYING'})
+   client.user.setActivity("DS$play",{type: 'PLAYING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
